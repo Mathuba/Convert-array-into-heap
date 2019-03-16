d7044d6eee657e7e29ee62430129a86e3bcbcf40
Mathuba <mathuba58@gmail.com>
2019-03-17 01:13:50 +0200

    Removed debug statements and redundant code

--- build_heap.py
+++ build_heap.py
@@ -8,12 +8,9 @@ class HeapBuilder:
         self._size = 0
 
     def read_data(self):
-        # n = int(input())
-        n = 5
-        # self._data = [int(s) for s in input().split()]
-        self._data = [1, 2, 3, 4, 5]
+        n = int(input())
+        self._data = [int(s) for s in input().split()]
         assert n == len(self._data)
-        self._size = n - 1
 
     def write_response(self):
         print(len(self._swaps))
@@ -39,7 +36,7 @@ class HeapBuilder:
 
         right_child_index = self.right_child(index_i)
         if (left_child_index < self._size) and (self._data[right_child_index] < self._data[min_index]):
-             min_index = right_child_index
+            min_index = right_child_index
 
         if index_i != min_index:
             self.swap(index_i, min_index)
