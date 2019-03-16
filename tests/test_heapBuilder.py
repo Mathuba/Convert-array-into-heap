261b6abd1bf586bec609806a74035d42a3dedf55
Mathuba <mathuba58@gmail.com>
2019-03-16 15:07:35 +0200

    Added test cases for left child method

--- tests/test_heapBuilder.py
+++ tests/test_heapBuilder.py
@@ -22,36 +22,42 @@ def test_unsorted_heap(heap_builder):
 
 
 def test_sorted_heap(heap_builder):
+    heap_builder._data.clear()
     heap_builder._data = [1, 2, 3, 4, 5]
     assert heap_builder._data == [1, 2, 3, 4, 5]
 
 
 def test_generate_old_swap(heap_builder):
+    heap_builder._data.clear()
     heap_builder._data = [5, 4, 3, 2, 1]
     heap_builder.generate_swaps()
     assert heap_builder._swaps == [(0, 1), (0, 2), (0, 3), (0, 4), (1, 2), (1, 3), (1, 4), (2, 3), (2, 4), (3, 4)]
 
 
 def test_generate_old_sorted_heap(heap_builder):
+    heap_builder._data.clear()
     heap_builder._data = [1, 2, 3, 4, 5]
     heap_builder.generate_swaps()
     assert heap_builder._swaps == []
 
 
 def test_build_heap(heap_builder):
+    heap_builder._data.clear()
     heap_builder._data = [5, 4, 3, 2, 1]
     heap_builder.build_heap()
     assert heap_builder._swaps == [(1, 4), (0, 1), (1, 3)]
 
 
 def test_build_heap_on_sorted_list(heap_builder):
+    heap_builder._data.clear()
     heap_builder._data = [1, 2, 3, 4, 5]
     heap_builder.build_heap()
     assert heap_builder._swaps == []
 
 
 def test_parent_of_left_child(heap_builder):
-    heap_builder._data = [5, 4, 3, 2, 1]
+    heap_builder._data.clear()
+    heap_builder._data = [100, 19, 36, 17, 3, 25, 1, 2, 7]
     left_child_parent = heap_builder.parent(1)
     assert left_child_parent == 0
 
@@ -69,4 +75,31 @@ def test_parent_of_left_most_leaf(heap_builder):
 def test_parent_of_right_most_leaf(heap_builder):
     right_most_parent_of_child = heap_builder.parent(4)
     assert right_most_parent_of_child == 1
+
+
+def test_left_child_of_root(heap_builder):
+    left_child = heap_builder.left_child(0)
+    assert left_child == 1
+
+
+def test_left_child_of_uppermost_left_child_node(heap_builder):
+    left_child = heap_builder.left_child(1)
+    assert left_child == 3
+
+
+def test_left_child_of_uppermost_right_child_node(heap_builder):
+    left_child = heap_builder.left_child(2)
+    assert left_child == 5
+
+
+def test_left_child_of_a_middle_node(heap_builder):
+    left_child = heap_builder.left_child(3)
+    assert left_child == 7
+
+
+def test_invalid_left_child(heap_builder):
+    left_child = heap_builder.left_child(4)
+    node_in_heap = left_child <= heap_builder._size
+    assert node_in_heap is False
+
 
